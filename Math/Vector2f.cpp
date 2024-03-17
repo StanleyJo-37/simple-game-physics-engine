80cf1e59b97658d743c11e4eb0fdd0e173ef033e
Stanley Jonathan Wahjudi <136320258+StanleyJo-37@users.noreply.github.com>
2024-03-17 23:55:20 +0700

    Update Vector2f.cpp

--- Math/Vector2f.cpp
+++ Math/Vector2f.cpp
@@ -1,1 +1,32 @@
 #include "Vector2f.h"
+
+Vector2f::Vector2f(const Vector2f& other)
+{
+	m_X = other.getX();
+	m_Y = other.getY();
+}
+
+Vector2f& Vector2f::operator=(const Vector2f& other)
+{
+	if (this != &other)
+	{
+		this->m_X = other.getX();
+		this->m_Y = other.getY();
+	}
+	return *this;
+}
+
+Vector2f Vector2f::projection(const Vector2f& vec, const Vector2f& line)
+{
+	float angle = angleDegree(vec, line);
+
+	return (vec * (line.norm() / (line.norm() * line.norm())));
+}
+
+// Non-member function definition
+
+const ostream& operator<<(ostream& out, const Vector2f& vec)
+{
+	out << "(" << vec.m_X << ", " << vec.m_Y << ")";
+	return out;
+}
